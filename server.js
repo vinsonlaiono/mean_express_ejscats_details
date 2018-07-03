e8f667d982ba0de150c87a2c95fccae24a03d967
vinson aiono <vinson.aiono@gmail.com>
2018-07-03 16:53:58 -0700

    corrected nested for loop

--- server.js
+++ server.js
@@ -10,42 +10,25 @@ app.get('/', function(request, response) {
 
 app.get('/details/1', function(request, response) {
     var cat_details =[
-        {image : '/images/lion.jpg', name: 'Lion',fav_food: 'monkeys', age: '3'}
+        {image : '/images/lion.jpg', name: 'Lion',fav_food: 'monkeys', age: '3', sleep: ['caves', 'trees']}
     ]
-    var spots = [
-        {
-            spots: 'trees',
-            spots: 'grounds'
-        }
-    ]
-    response.render('details.ejs', {cat: cat_details, spot: spots})
+    response.render('details.ejs', {cat: cat_details})
 })
 app.get('/details/2', function(request, response) {
     var cat_details =[
-        {image : '/images/tiger.jpg', name: 'Tiger',fav_food: 'Apes', age: '5'},
+        {image : '/images/tiger.jpg', name: 'Tiger',fav_food: 'Apes', age: '5'}
     ]
-    var spots = [
-        {
-            spots: 'trees',
-            spots: 'tables'
-        }
-    ]
-    response.render('details.ejs', {cat: cat_details, spot: spots})
+    response.render('details.ejs', {cat: cat_details})
 })
 app.get('/details/3', function(request, response) {
     var cat_details =[
-        {image : '/images/panther.jpg', name: 'Panther',fav_food: 'Chickens', age: '12'},
+        {image : '/images/panther.jpg', name: 'Panther',fav_food: 'hogs', age: '3'}
     ]
-    var spots = [
-        {
-            spots: 'Logs',
-            spots: 'homes'
-        }
-    ]
-    response.render('details.ejs', {cat: cat_details, spot: spots})
+    response.render('details.ejs', {cat: cat_details})
 })
 
 
 app.listen(8000, function() {
     console.log("listening on port 8000");
 })
+
